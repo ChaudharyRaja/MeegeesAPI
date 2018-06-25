74c12b363e35adc73917c18075f5869a78cb066f
rajachaudhary <raja.chaudhary@daffodilsw.com>
2018-06-25 08:34:27 +0530

    first commit

---
+++ meegees_api/meegees_api/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace meegees_api.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ meegees_api/meegees_api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="meegees_api.WebApiApplication" Language="C#" %>
